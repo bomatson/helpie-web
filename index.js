8975192f4fa82dc84fbd9a9bf26080f557f83753
Rudy Jahchan <rudy@carbonfive.com>
2015-09-10 03:40:48 -0700

    Download audio

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
-var models = require('./models');
 var getRawBody = require('raw-body');
 var wit = require('node-wit');
+var fs = require('fs');
+var streamifier = require('streamifier');
+
+var models = require('./models');
 
 var ACCESS_TOKEN = 'EEBIDF2KTIIDA574WDZP7NUYPSVLG6IG';
 
@@ -9,24 +12,44 @@ const app = express();
 
 const server = app
   .post('/checkins', function(request, response) {
+    var responseText = "";
     getRawBody(request, {
     }, function (err, string) {
       if (err) {
         response.end('ERROR!');
       }
-      response.send('Upload length = ' + string.length);
+      responseText = responseText + 'Upload length = ' + string.length;
 
-      var stream = fs.createReadStream('/Users/rudy/sample.wav');
+      var stream = streamifier.createReadStream(string);
       wit.captureSpeechIntent(ACCESS_TOKEN, stream, "audio/wav", function (err, res) {
-        response.send("Response from Wit for audio stream: ");
+        responseText = responseText + "Response from Wit for audio stream: ";
         if (err) response.end("Error: ", err);
-        response.end(JSON.stringify(res, null, " "));
+        responseText = responseText + JSON.stringify(res, null, " ");
+        models.CheckIn.create({name: 'a-test', location: 'this place', audio: string}).then(function(checkIn){
+          responseText = responseText + "SAVED";
+          response.end(responseText);
+        })
+        .catch(function(error) {
+          response.end(errror);
+        });;
       });
     })
   })
+  .get('/checkins/:id/audio', function(request, response) {
+    models.CheckIn.findById(request.params.id)
+      .then(function(checkIn) {
+        response.set('Content-Type', 'audio/wav');
+        response.send(checkIn.audio);
+      });
+  })
   .get('/checkins', function(request,response) {
     models.CheckIn.findAll().then(function(checkIns) {
-      response.end("Number of CheckIns = " + checkIns.length);
+      responseText = "<ul>";
+      checkIns.forEach(function(checkIn) {
+        responseText = responseText + "<li>" + checkIn.id + "</li>";
+      });
+      responseText = responseText + "</ul>";
+      response.end(responseText);
     });
   });
 
